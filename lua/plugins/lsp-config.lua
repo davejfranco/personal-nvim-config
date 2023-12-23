67ee06151b7da25f05dfc46ced2abd6af542f060
Dave <davefranco1987@gmail.com>
2023-12-23 13:16:59 +0100

    lsp installation and config

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,81 @@
+-- To get helm on lsp :h vim.lsp.buf
+return {
+  {
+    -- LSP installation 
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    -- Default lsp's for DevOps work
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "terraformls",
+          "ansiblels",
+          "dockerls",
+          "helm_ls",
+          "pyright",
+          "yamlls",
+          "lua_ls",
+          "gopls",
+        }
+      })
+    end
+  },
+  {
+    -- This allows to config and use the lsp
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require('lspconfig')
+      lspconfig.terraformls.setup({})
+      lspconfig.ansiblels.setup({})
+      lspconfig.dockerls.setup({})
+      lspconfig.helm_ls.setup({})
+      lspconfig.pyright.setup({})
+      lspconfig.yamlls.setup({})
+      lspconfig.lua_ls.setup({})
+      lspconfig.gopls.setup({})
+      -- keymap
+      -- Global mappings.
+      -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+      vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+      vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+      vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+      -- Use LspAttach autocommand to only map the following keys
+      -- after the language server attaches to the current buffer
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+          -- Enable completion triggered by <c-x><c-o>
+          vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+          -- Buffer local mappings.
+          -- See `:help vim.lsp.*` for documentation on any of the below functions
+          local opts = { buffer = ev.buf }
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+          vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+          vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+          vim.keymap.set('n', '<leader>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          end, opts)
+          vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+          vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+          vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+          vim.keymap.set('n', '<leader>f', function()
+            vim.lsp.buf.format { async = true }
+          end, opts)
+        end,
+      })
+    end
+  }
+}
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,8 +1,9 @@
+vim.cmd("set number")
 -- Tab settings
--- Setting tab settings to 4 space, might regret 
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+-- Leader 
 vim.g.mapleader = " "
 
